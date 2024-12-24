24a16634cec2658f10a341907420c708ddc2f14c
firas najjar <162709181+firas988@users.noreply.github.com>
2024-12-24 11:41:58 +0200

    Add files via upload

---
+++ Server/proj/app.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const dirPath = path.join(__dirname, "/templates");
+
+//read html file
+let file2send = fs.readFileSync(`${dirPath}/page.html`);
+
+const server = http.createServer((req, res) => {
+  //setting the header content type to html
+  res.setHeader("Content-type", "text/html");
+  //handle incoming requests here.
+  console.log(req);
+  //sending html content
+  res.write(file2send);
+  res.write("aaa");
+  //ending
+  res.end("<br/> this is html");
+});
+
+server.listen(3000, () => {});
